606c0f3e557ae648119c5a03cdf55cf997f7e6d0
Sterling Greene <sterling@gradle.com>
2019-07-15 15:09:02 -0400

    Revert "Patch teamcity.build.tempDir property"
    This reverts commit f80a5975b4329f6625a86eb45b8a73cf69e05384.

--- build.gradle.kts
+++ build.gradle.kts
@@ -189,9 +189,6 @@ apply(plugin = "gradlebuild.update-versions")
 apply(plugin = "gradlebuild.dependency-vulnerabilities")
 apply(plugin = "gradlebuild.add-verify-production-environment-task")
 
-// https://github.com/gradle/gradle-private/issues/2463
-apply(from = "gradle/remove-teamcity-temp-property.gradle")
-
 allprojects {
     apply(plugin = "gradlebuild.dependencies-metadata-rules")
 }
@@ -397,4 +394,3 @@ allprojects {
         fileMode = Integer.parseInt("0644", 8)
     }
 }
-
